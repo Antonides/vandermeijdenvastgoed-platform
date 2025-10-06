d2f2a6878c343787308f12d40eee7a7d9c4ee5f2
Michel <michel@michelantonides.com>
2025-10-06 11:25:31 +0200

    user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             FilamentAdminSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'mark@vandermeijdenvastgoed.nl'],
+            [
+                'name' => 'Mark van der Meijden',
+                'email' => 'mark@vandermeijdenvastgoed.nl',
+                'password' => Hash::make('Tripple20'),
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+}
