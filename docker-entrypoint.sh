aa88992296f462ab558cd44461c17ecae903a4d9
Michel <michel@michelantonides.com>
2025-11-23 23:03:22 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,3 +5,6 @@ su -s /bin/bash -c "php artisan config:cache" www-data
 su -s /bin/bash -c "php artisan route:cache" www-data
 su -s /bin/bash -c "php artisan view:cache" www-data
 su -s /bin/bash -c "php artisan event:cache" www-data
+
+# Run migrations
+su -s /bin/bash -c "php artisan migrate --force" www-data
